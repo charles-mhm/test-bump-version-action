6288d9484c12c6a0591fd09989a95a526b910d2a
Charles Loveman <cl@manholemetrics.com>
2024-10-18 13:18:31 +0100

    fix

--- bump_version.sh
+++ bump_version.sh
@@ -1,1 +1,7 @@
-2
+curl -L \
+    -X PATCH \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $TOKEN"\
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/charles-mhm/test-bump-version-action/actions/variables/PATCH \
+    -d {"name":"$NAME","value":"$VALUE"}
